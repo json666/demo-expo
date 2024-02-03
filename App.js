a10b9194d252c714a9307ecb1317483dd87614c5
jheyson <json.dev.sanchez@gmail.com>
2024-02-02 22:11:41 -0400

    clase 1

--- App.js
+++ App.js
@@ -10,12 +10,18 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      
-      <TodoCount tipo={1} total={50}/>
-      <TodoCounter/>
-      <TodoList/>
-      <TodoSearch/>
-      <TodoItems/>
+
+      <TodoCount tipo={1} total={50} />
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        <TodoItems />
+        <TodoItems />
+        <TodoItems />
+        <TodoItems />
+        <TodoItems />
+      </TodoList>
+        Hacer un componente Boton
       <StatusBar style="auto" />
     </View>
   );
@@ -30,7 +36,10 @@ const styles = StyleSheet.create({
   },
 });
 
-
 function TodoCount(props) {
-  return <h1>{props.tipo}--{props.total}</h1>;
+  return (
+    <h1>
+      {props.tipo}--{props.total}
+    </h1>
+  );
 }
